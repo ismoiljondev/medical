22d72dbc1d45a1b89af598f40c21062be6a29076
Кот Странник <firegamesboy0220@gmail.com>
2024-05-16 16:46:17 +0500

    removed trash lines from code

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-// import { SignupForm } from "./components/login/login";
 import Showcase from "./components/Showcase/Showcase";
-// import Title from "@/components/Title/Title";
 
 function App() {
   return (
     <>
-      {/* <div className="container py-20">
-        <Title
-          types="showcase, normal_headline"
-          header="line"
-          headline="Headline"
-          subHeadline="SubHeadline"
-          link="/catalog"
-        />
-      </div> */}
       <Routes>
         <Route element={<Showcase />} path="*" />
       </Routes>
