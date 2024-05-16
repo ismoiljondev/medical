c7df618a1ab508bc730ca093de103373d4c73c22
Ismoil <ismoil997@seznam.cz>
2024-05-16 11:42:46 +0500

    init: add component files

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { SignupForm } from "./components/login/login";
+// import { SignupForm } from "./components/login/login";
+import Services from "./components/med-services/Services";
 
 function App() {
-	return (
-		<Routes>
-			<Route element={<SignupForm />} path="*" />
-		</Routes>
-	);
+  return <Services />;
 }
 
 export default App;
---
+++ src/components/med-services/Services.jsx
@@ -0,0 +1,13 @@
+import Card from "./Card";
+
+function Services() {
+  return (
+    <div className="bg-azure h-[60px] border">
+      {/* <div className="container"> */}
+      <Card />
+      {/* </div> */}
+    </div>
+  );
+}
+
+export default Services;
