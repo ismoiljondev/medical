a56bc409bede3053b83e422a5ec9a946f3d5bb96
Zafarov Polat <atuin59354081@gmail.com>
2024-05-17 16:05:56 +0500

    Fixed path to Register.tsx on App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { SignupForm } from "./components/Register/login";
+import { SignupForm } from "./components/Register/Register";
 
 function App() {
   return (
