801dd7a65d48ac7fded9379195573fe481267366
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2024-05-18 15:09:06 +0500

    showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { SignupForm } from "./components/Register/Register";
+import Showcase from "./components/Showcase/Showcase";
 
 function App() {
 	return (
-		<Routes>
-			<Route element={<SignupForm />} path="*" />
-		</Routes>
+		<>
+			<Showcase />
+			<Routes>
+				<Route element={<SignupForm />} path="/login" />
+			</Routes>
+		</>
 	);
 }
 
