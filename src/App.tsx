f05f98ec3f2db107713769bdfd64b8a920694003
Entony <entony@mail.ru>
2024-05-18 16:47:38 +0500

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { SignupForm } from "./components/Register/Register";
-import Ourteam from "./components/ourteam/ourteam";
 
 function App() {
   return (
     <Routes>
       <Route element={<SignupForm />} path="*" />
-	  <Route element={<Ourteam/>} path="doctors"/>
     </Routes>
   );
 }
