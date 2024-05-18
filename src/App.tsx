8517adaafd9efceec9039c147eddabde653e9154
Ismoiljondev <ismoiljonhalilov5@gmail.com>
2024-05-18 14:54:27 +0500

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { SignupForm } from "./components/Register/login";
+import { SignupForm } from "./components/Register/Register";
 
 function App() {
-  return (
-    <Routes>
-      <Route element={<SignupForm />} path="*" />
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route element={<SignupForm />} path="*" />
+		</Routes>
+	);
 }
 
 export default App;
