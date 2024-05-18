14b3f1b26aacea6efe48762c0748376aedb165d9
Zafarov Polat <atuin59354081@gmail.com>
2024-05-18 17:47:47 +0500

    Advice section and CommitCards are absolutly done

--- src/components/Advice/Advice.tsx
+++ src/components/Advice/Advice.tsx
@@ -7,28 +7,28 @@ import AuthorTwoImg from "@/img/author2.png"
 export const Advice = () => {
     return (
         <section className="advice">
-            <div className="container">
+            <div className="container max-w-[1050px]">
                 <Title
                     types="showcase"
                     header="Practice Advice"
                     headline="Leading Medicine"
                     subHeadline="Problems trying to resolve the conflict between 
                     the two major realms of Classical physics: Newtonian mechanics "
-                    className="mb-20"
+                    className="mb-20 max-[1366px]:mb-16 max-[800px]:mb-14 max-[600px]:mb-4 max-[600px]:flex max-[600px]:flex-col max-[600px]:justify-center max-[600px]:items-center max-[600px]:text-center"
                 />
 
-                <ul className="advice-cards grid grid-cols-3 justify-center items-center">
-                    <li className="advice-card">
+                <ul className="advice-cards grid grid-cols-3 justify-center items-center max-[600px]:grid-cols-1 ">
+                    <li className="advice-card max-[600px]:flex max-[600px]:items-center max-[600px]:justify-center">
                         <CommitCard rate={4}
                             commit={"Slate helps you see how many more days you need to work to reach your financial goal for the month and year."}
                             authorImg={AuthorOneImg} authorName={"Tyler Durden"} authorRole={"Designer"}></CommitCard>
                     </li>
-                    <li className="advice-card">
+                    <li className="advice-card max-[600px]:flex max-[600px]:items-center max-[600px]:justify-center">
                         <CommitCard rate={3}
                             commit={"Slate helps you see how many more days you need to work to reach your financial goal for the month and year."}
                             authorImg={AuthorTwoImg} authorName={"John Wick"} authorRole={"Administrator"}></CommitCard>
                     </li>
-                    <li className="advice-card">
+                    <li className="advice-card max-[600px]:flex max-[600px]:items-center max-[600px]:justify-center">
                         <CommitCard rate={1}
                             commit={"Slate helps you see how many more days you need to work to reach your financial goal for the month and year."}
                         ></CommitCard>
--- src/components/Commit-cards/CommitCard.tsx
+++ src/components/Commit-cards/CommitCard.tsx
@@ -14,9 +14,9 @@ const CommitCard = ({ rate, commit, authorImg = NoUserImg, authorName = "John Do
     let stars = [1, 2, 3, 4, 5]
 
     return (
-        <div className="commit-card flex flex-col justify-center items-center max-w-[328px] py-8">
+        <div className="commit-card flex flex-col justify-center items-center max-w-[328px] py-8 max-[600px]:py-6 max-[600px]:border-b">
 
-            <div className="commit-card-rate flex gap-1 text-[#F3CD03] mb-4">
+            <div className="commit-card-rate flex gap-1 text-[#F3CD03] mb-4 max-[720px]:mb-3 max-[600px]:text-[14px]">
                 {stars.map((i) => {
                     if ((i - 1) < Math.round(rate)) {
                         return <div><i className="fa-solid fa-star"></i></div>
@@ -27,7 +27,7 @@ const CommitCard = ({ rate, commit, authorImg = NoUserImg, authorName = "John Do
                 })}
             </div>
 
-            <div className="commit-card-text text-center max-w-[159px] font-[Montserrat] mb-8 text-[14px] text-[#737373] font-medium">
+            <div className="commit-card-text text-center max-w-[159px] font-[Montserrat] mb-8 text-[14px] text-[#737373] font-medium max-[720px]:text-[13px] max-[720px]:mb-6 max-[600px]:text-[12px] max-[600px]:mb-4">
                 {commit}
             </div>
 
@@ -37,8 +37,8 @@ const CommitCard = ({ rate, commit, authorImg = NoUserImg, authorName = "John Do
                 </div>
 
                 <div className="commit-card-author-info font-[Montserrat] font-bold">
-                    <h5 className="commit-card-author-info-name text-[#FF685B] text-[14px]">{authorName}</h5>
-                    <p className="commit-card-author-info-role text-[#252B42] text-[12px]">{authorRole}</p>
+                    <h5 className="commit-card-author-info-name text-[#FF685B] text-[14px] max-[600px]:text-[12px]">{authorName}</h5>
+                    <p className="commit-card-author-info-role text-[#252B42] text-[12px] max-[600px]:text-[11px]">{authorRole}</p>
                 </div>
             </div>
 
